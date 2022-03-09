73c9af4a90d9fa44dd16104bffc34eaa07c8c37f
AriAndiM <andyary961@gmail.com>
2022-03-09 21:34:22 +0700

    Menambahkan file variabel.php

---
+++ variabel.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a=6;
+    $b=8;
+    $c=10;
+
+    echo "<br/> PENJUMLAHAN<br/>";
+    $penjumlahan=$a+$c;
+    echo $a,"+",$c,"=",$penjumlahan;
+
+    echo "<br/> PENGURANGAN<br/>";
+    $pengurangan=$c-$b;
+    echo $c,"-",$b,"=",$pengurangan;
+
+    echo "<br/> PERKALIAN<br/>";
+    $perkalian=$a*$c;
+    echo $a,"*",$c,"=",$perkalian;
+
+    ?>
+</body>
+</html>
