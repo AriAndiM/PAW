cc6cd88bda619b4f6493f9c942f557b7f1c42a9e
AriAndiM <andyary961@gmail.com>
2022-04-18 16:10:32 +0700

    menambah file pngtree-classroom-icon-for-your-project-png-image_1533148-removebg-preview.png,siswa 10 ipa 1.php,sukses.php,update siswa x ipa 1.php

---
+++ siswa 10 ipa 1.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <title>Data Siswa 10 IPA 1</title>
+</head>
+<body bgcolor="#E45826">
+      <!-- Sidebar -->
+      <div class="w3-sidebar w3-dark-grey w3-bar-block" style="width:20%;padding-top:80px;">
+      <a href="home.php" class="w3-bar-item w3-button">Home</a>
+      <a href="opsi kelas siswa .php" class="w3-bar-item w3-button">Data Kelas</a>
+      <a href="opsi kelas jadwal.php" class="w3-bar-item w3-button">Jadwal Mapel</a>
+      <a href="opsi jurusan.php" class="w3-bar-item w3-button">Mata Pelajaran</a>
+      <a href="login.php" class="w3-bar-item w3-button">Logout</a>
+    </div>
+
+    <!-- Page Content -->
+    <div style="margin-left:20%">
+
+    <div class="w3-container" style="text-align: center;">
+    <h1 style="text-align: center;font-family:Fira Sans;color:#E6D5B8;"><b>Daftar Siswa Kelas X IPA 1</b></h1>
+    <!-- <a href="form edit siswa kelas x ipa 1.php">Edit</a> -->
+    <button style="background-color: #008CBA;border-radius:5px;"><a href="form tambah siswa x ipa 1.php" style="text-decoration:none;">Tambah</a></button>
+
+    <div style="padding-top: 20px;">
+        <table class="w3-table-all">
+            <tr>
+                <th>Id</th>
+                <th>NISN</th>
+                <th>Nama Siswa</th>
+                <th>Action</th>
+            </tr>
+            <?php
+
+            include "koneksi.php";
+
+            $query="SELECT * FROM kelas";
+            $hasil=mysqli_query($conn,$query);
+
+            while($row=mysqli_fetch_array($hasil))
+            {
+            ?>
+
+            <tr>
+                <td><?php echo $row['Id'] ?></td>
+                <td><?php echo $row['NISN'] ?></td>
+                <td><?php echo $row['NamaSiswa'] ?></td>
+                <td>
+                    <a style="text-decoration: none;color:#008CBA" href="form edit siswa kelas x ipa 1.php?Id=<?php echo $row['Id'];?>"><span class="badge bg-primary">Edit</span></a>
+                    |
+                    <a style="text-decoration: none;color:#008CBA" href="delete siswa x ipa 1.php?Id=<?php echo $row['Id'];?>"><span class="badge bg-danger">Delete</span></a>  
+                <td>
+            </tr>
+            <?php } ?>
+        </table>
+
+    </div>
+    </div>
+    </div>
+
+</body>
+</html>
---
+++ update siswa x ipa 1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Siswa</title>
+</head>
+<body style="background-color:#E45826;">
+<div style="background-color:#ccccb3;font-size:35px;font-weight:bold;font-family:bradley-hand,cursive;text-align:center;height:200px; width:1150px;margin:60px;border-radius:10px;">
+        <?php
+                include "koneksi.php";  
+                $id=$_POST['Id'];
+                $nisn=$_POST['NISN'];
+                $nama=$_POST['NamaSiswa'];
+        
+                $sql="UPDATE kelas SET NISN='$nisn',NamaSiswa='$nama' WHERE Id='$id'";
+                $hasil=mysqli_query($conn,$sql);
+                if(!$hasil){
+                        echo "gagal Mengubah Data Siswa";
+                } else {
+                        echo "Berhasil Mengubah Data Siswa";       
+                }
+        ?>
+        <div style="padding-top:20px;">
+                <button type="link" style="background-color: #E45826;width:80px;height:40px;border-radius:10%;"><a href="siswa 10 ipa 1.php" style="color: #ccccb3;text-decoration:none;font-size:20px;">Back</a></button>
+                <button type="link" style="background-color: #E45826;width:80px;height:40px;border-radius:10%;"><a href="home.php" style="color: #ccccb3;text-decoration:none;font-size:20px;">Home</a></button>
+        </div>
+        <div>
+                <img src="sukses.png">
+        </div>
+</div>
+</body>
+</html>
+
+
+
