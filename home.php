5b683cb91b7565eacddd2ef7cbe26c7cd341d1e5
AriAndiM <andyary961@gmail.com>
2022-04-18 16:01:30 +0700

    home.php

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <title>Home</title>
+</head>
+<body bgcolor="#E45826">
+      <!-- Sidebar -->
+      <div class="w3-sidebar w3-dark-grey w3-bar-block" style="width:20%;padding-top:80px;">
+      <a href="home.php" class="w3-bar-item w3-button">Home</a>
+      <a href="opsi kelas siswa .php" class="w3-bar-item w3-button">Data Kelas</a>
+      <a href="opsi kelas jadwal.php" class="w3-bar-item w3-button">Jadwal Mapel</a>
+      <a href="opsi jurusan.php" class="w3-bar-item w3-button">Mata Pelajaran</a>
+      <a href="login.php" class="w3-bar-item w3-button">Logout</a>
+    </div>
+
+    <!-- Page Content -->
+    <div style="margin-left:20%">
+
+    <div class="w3-container w3-white">
+        <img src="https://scontent.fsub4-1.fna.fbcdn.net/v/t39.30808-6/278263215_529035988749425_5346217876266370131_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeGMIxm2QfCSYfQ2K7-3lLEg_rPgMPeCDcf-s-Aw94INxyxopYAP-2Cs9MRyQ5VNSA-cjx_eQpA2K0PfVVAJG3fI&_nc_ohc=lyQwyhaLTrUAX8xsi39&tn=xUedOuH0mZhkXa5Q&_nc_zt=23&_nc_ht=scontent.fsub4-1.fna&oh=00_AT-kF5zHJl9yesBc4Bv9EcKn0HwTMw-jPDrTd_L7E5XkrQ&oe=625CF0A2" style="width: 40px;height:40px;margin-left:50%;margin-top:10px;margin-bottom:0px;">
+      <h1 style="text-align: center;font-family:Fira Sans;"><b>SMA Islam Temayang</b></h1>
+    </div>
+
+    <div class="w3-container" style="padding: 40px;">
+      <img src="https://scontent.fsub3-1.fna.fbcdn.net/v/t39.30808-6/278585965_530161375303553_1374758624142554918_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeGbqDEPOW0lzCX8Qkq04J2kJtlxvA2cYAom2XG8DZxgCv7dke_tQsA5zuPwT0crz81Vlj7k020MlkgdREvsRSK_&_nc_ohc=bjz0FV_IEzEAX_YQ95P&_nc_zt=23&_nc_ht=scontent.fsub3-1.fna&oh=00_AT8GiwB5RnVQ9u-TTRX-qOSbQy8rYB_mCG22g8f3vdy3cA&oe=625CA7F5" alt="" style="width:100%;height:100%;" >
+    </div>
+
+    </div>
+      
+</body>
+</html>
