ed7c0f50c697e397f245003c187b042a514e3472
AriAndiM <andyary961@gmail.com>
2022-04-18 16:07:56 +0700

    opsi jurusan.php,opsi kelas jadwal.php,opsi kelas siswa.php

---
+++ opsi jurusan.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <title>Data Kelas</title>
+</head>
+<body bgcolor="#E45826">
+      <!-- Sidebar -->
+      <div class="w3-sidebar w3-dark-grey w3-bar-block" style="width:20%;padding-top:80px;">
+      <a href="home.php" class="w3-bar-item w3-button">Home</a>
+      <a href="opsi kelas siswa.php" class="w3-bar-item w3-button">Data Kelas</a>
+      <a href="opsi kelas jadwal.php" class="w3-bar-item w3-button">Jadwal Mapel</a>
+      <a href="opsi jurusan.php" class="w3-bar-item w3-button">Mata Pelajaran</a>
+      <a href="login.php" class="w3-bar-item w3-button">Logout</a>
+    </div>
+
+    <!-- Page Content -->
+    <div style="margin-left:20%">
+
+    <div class="w3-container" style="text-align: center;padding-top:70px;">
+    <h1 style="text-align: center;font-family:Fira Sans;color:#E6D5B8;"><b>Daftar Mapel Kelas </b></h1>
+
+    <div style="padding-left: 40px;padding-right: 40px;padding-bottom: 40px;padding-top:40px;">
+        <a href="mapel kelas  ipa.php" class="w3-btn w3-yellow">IPA</a>
+    </div>
+    <div style="padding-left: 40px;padding-right: 40px;padding-bottom: 40px;">
+        <a href="mapel kelas ips.php" class="w3-btn w3-yellow">IPS</a>
+    </div>
+ 
+    </div>
+    </div>
+      
+</body>
+</html>
---
+++ opsi kelas jadwal.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <title>Data Kelas</title>
+</head>
+<body bgcolor="#E45826">
+      <!-- Sidebar -->
+      <div class="w3-sidebar w3-dark-grey w3-bar-block" style="width:20%;padding-top:80px;">
+      <a href="home.php" class="w3-bar-item w3-button">Home</a>
+      <a href="opsi kelas siswa .php" class="w3-bar-item w3-button">Data Kelas</a>
+      <a href="opsi kelas jadwal.php" class="w3-bar-item w3-button">Jadwal Mapel</a>
+      <a href="opsi jurusan.php" class="w3-bar-item w3-button">Mata Pelajaran</a>
+      <a href="login.php" class="w3-bar-item w3-button">Logout</a>
+    </div>
+
+    <!-- Page Content -->
+    <div style="margin-left:20%">
+
+    <div class="w3-container" style="text-align: center;padding-top:70px;">
+    <h1 style="text-align: center;font-family:Fira Sans;color:#E6D5B8;"><b>Daftar Jadwal Kelas </b></h1>
+
+    <div style="padding-left: 40px;padding-right: 40px;padding-bottom: 40px;padding-top:40px;">
+        <a href="jadwal mapel x ipa 1.php" class="w3-btn w3-yellow">X IPA 1</a>
+        <a href="#" class="w3-btn w3-white">XI IPA 1</a>
+        <a href="#" class="w3-btn w3-yellow">XII IPA 1</a>
+    </div>
+    <div style="padding-left: 40px;padding-right: 40px;padding-bottom: 40px;">
+        <a href="#" class="w3-btn w3-yellow">X IPA 2</a>
+        <a href="#" class="w3-btn w3-white">XI IPA 2</a>
+        <a href="#" class="w3-btn w3-yellow">XII IPA 2</a>
+    </div>
+    
+    <div style="padding-left: 40px;padding-right: 40px;padding-bottom: 40px;">
+        <a href="#" class="w3-btn w3-yellow">X IPS 1</a>
+        <a href="#" class="w3-btn w3-white">XI IPS 1</a>
+        <a href="#" class="w3-btn w3-yellow">XII IPS 1</a>
+    </div>
+
+    <div style="padding-left: 40px;padding-right: 40px;padding-bottom: 40px;">
+        <a href="#" class="w3-btn w3-yellow">X IPS 2</a>
+        <a href="#" class="w3-btn w3-white">XI IPS 2</a>
+        <a href="#" class="w3-btn w3-yellow">XII IPS 2</a>
+    </div>
+
+    </div>
+    </div>
+      
+</body>
+</html>
