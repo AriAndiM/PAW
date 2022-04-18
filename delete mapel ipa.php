4a17f661a983e1e1a0a2c198d2c518f2ae7afc04
AriAndiM <andyary961@gmail.com>
2022-04-18 15:57:01 +0700

    menambahkan file delete jadwal 10 ipa 1.php,delete mapel ipa.php,delete siswa x ipa 1.php

---
+++ delete mapel ipa.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete</title>
+</head>
+<body style="background-color:#E45826;">
+<div style="background-color:#ccccb3;font-size:35px;font-weight:bold;font-family:bradley-hand,cursive;text-align:center;height:200px; width:1150px;margin:60px;border-radius:10px;">
+        <?php
+                include "koneksi.php";  
+                $id=$_GET['Id_Mapel'];
+                $sql= "DELETE FROM mapel WHERE Id_Mapel=$id";
+                $hasil= mysqli_query($conn,$sql);
+                if(!$hasil){
+                        echo "gagal Menghapus Mata Pelajaran";
+                } else {
+                        echo "Berhasil Menghapus Mata Pelajaran";       
+                }
+        ?>
+        <div style="padding-top:20px;">
+                <button type="link" style="background-color: #E45826;width:80px;height:40px;border-radius:10%;"><a href="mapel kelas  ipa.php" style="color: #ccccb3;text-decoration:none;font-size:20px;">Back</a></button>
+                <button type="link" style="background-color: #E45826;width:80px;height:40px;border-radius:10%;"><a href="home.php" style="color: #ccccb3;text-decoration:none;font-size:20px;">Home</a></button>
+        </div>
+        <div>
+                <img src="sukses.png">
+        </div>
+</div>
+</body>
+</html>
